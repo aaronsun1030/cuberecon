85e1e051621bea7571fe9c299fc9772d65c3ec37
Aaron Sun <aaronsun2000@berkeley.edu>
2020-12-27 18:10:14 -0800

    aldsfjalf

--- gen_flow_data.py
+++ gen_flow_data.py
@@ -26,11 +26,9 @@ def process_video(file_path, mag_thresh):
         mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
         if np.average(mag) < mag_thresh:
             cur_label = "N"
-        hsv[...,0] = ang*180/np.pi/2
-        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-        rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        mag_ang = np.array([mag, ang])
 
-        flow_frames.append(rgb)
+        flow_frames.append(mag_ang)
         frame_labels.append(cur_label)
 
         prvs = next
@@ -40,30 +38,22 @@ def process_video(file_path, mag_thresh):
     return flow_frames, frame_labels
 
 def gen_dataset(mag_thresh = 0, frames_path = "./frame_data", videos_path = "./data"):
-    
+
+    label_counts = {}
     if os.path.isdir(frames_path):
         shutil.rmtree(frames_path)
     frames, frame_labels = [], []
+    os.mkdir(frames_path)
     for subdir, dirs, files in os.walk(videos_path):
         for file in files:
             if file.endswith(".mov"):
                 file_path = subdir + os.sep + file
                 print(file_path)
                 video_frames, video_labels = process_video(file_path, mag_thresh)
-                frames.extend(video_frames)
-                frame_labels.extend(video_labels)
-
-    os.mkdir(frames_path)
-    cats = list(set(frame_labels))
-
-    for cat in cats:
-        os.mkdir(frames_path + os.sep + cat)
-
-    label_counts = {}
-    for ind in range(len(frames)):
-        label = frame_labels[ind]
-        label_counts[label] = label_counts.get(label, 0) + 1
-        frame_path = frames_path + os.sep + label + os.sep + label + str(label_counts[label]) + ".png"
-        cv2.imwrite(frame_path, frames[ind])
-
+                for frame, label in zip(video_frames, video_labels):
+                    label_counts[label] = label_counts.get(label, 0) + 1
+                    if not os.path.exists(frames_path + os.sep + label):
+                        os.mkdir(frames_path + os.sep + label)
+                    frame_path = frames_path + os.sep + label + os.sep + label + str(label_counts[label]) + ".npy"
+                    np.save(frame_path, frame)
 gen_dataset(1)
