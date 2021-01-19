d047a19a65c27c8995519f93ecda0bd78388b624
Aaron Sun <aaronsun2000@berkeley.edu>
2021-01-18 17:32:18 -0800

    changed training params

--- tensorflow_pred.py
+++ tensorflow_pred.py
@@ -9,7 +9,6 @@ from datetime import datetime
 #[X, y]
 encoding = ['D', 'Dp', 'N', 'R', 'Rp', 'U', 'Up']
 #print(list(os.walk("frame_data/")))
-
 def batch(split):
     all_dirs = list(os.walk("frame_data/"))
     random.shuffle(all_dirs)
@@ -17,11 +16,12 @@ def batch(split):
         all_dirs = all_dirs[len(all_dirs) * 4 // 5:]
     else:
         all_dirs = all_dirs[:len(all_dirs) * 4 // 5]
-    for subdir, dirs, files in all_dirs:
-        for file in files:
-            if file.endswith(".npy"):
-                yield (tf.reshape(tf.image.resize(np.swapaxes(np.load(subdir + '/' + file), 0, 2), size=(256, 256)), (1, 256, 256, 2)), 
-                tf.reshape(tf.one_hot(encoding.index(subdir[subdir.index('/') + 1:]), len(encoding)), (1, len(encoding))))
+    while True:
+        for subdir, dirs, files in all_dirs:
+            for file in files:
+                if file.endswith(".npy"):
+                    yield (tf.reshape(tf.image.resize(np.swapaxes(np.load(subdir + '/' + file), 0, 2), size=(256, 256)), (1, 256, 256, 2)), 
+                    tf.reshape(tf.one_hot(encoding.index(subdir[subdir.index('/') + 1:]), len(encoding)), (1, len(encoding))))
 
 model = tf.keras.applications.InceptionV3(
     include_top=True,
@@ -40,5 +40,5 @@ logdir="logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
 model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.CategoricalCrossentropy())
-model.fit(x=batch("train"), epochs=5, steps_per_epoch=500, validation_data=batch("val"), validation_batch_size=100, validation_steps=5, callbacks=[tensorboard_callback])
+model.fit(x=batch("train"), epochs=5, steps_per_epoch=600, validation_data=batch("val"), validation_batch_size=100, validation_steps=5, callbacks=[tensorboard_callback])
 model.save("model")
