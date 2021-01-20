a3cbc125ae0be04bcff81e21f4535c9da3366bd0
Aaron Sun <aaronsun2000@berkeley.edu>
2021-01-19 18:23:08 -0800

    the maddest of merges

--- tensorflow_pred.py
+++ tensorflow_pred.py
@@ -6,9 +6,17 @@ import random
 import tensorboard
 from datetime import datetime
 
-#[X, y]
+def display_flow(mag, ang):
+    hsv = np.zeros((mag.shape[0], mag.shape[1], 3), dtype="uint8")
+    hsv[...,1] = 255
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+    cv2.imshow('frame2',rgb)
+    cv2.waitKey()
+
 encoding = ['D', 'Dp', 'N', 'R', 'Rp', 'U', 'Up']
-#print(list(os.walk("frame_data/")))
+
 def batch(split):
     all_dirs = list(os.walk("frame_data/"))[0][2]
     random.shuffle(all_dirs)
@@ -24,12 +32,12 @@ def batch(split):
                 X.append(tf.image.resize(np.swapaxes(np.load("frame_data/" + file), 0, 2), size=(256, 256)))
                 y.append(encoding.index(file[0] + ("p" if file[1] == "p" else "")))
                 if len(y) == 8:
-                    X = np.array(X)
+                    X = np.array(X, dtype="uint8")
                     y = tf.keras.utils.to_categorical(y, num_classes=len(encoding))
                     yield X, y
                     X, y = [], []
 
-model = tf.keras.applications.InceptionV3(
+'''model = tf.keras.applications.InceptionV3(
     include_top=True,
     weights=None,
     input_tensor=None,
@@ -49,4 +57,17 @@ tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 model = tf.keras.models.load_model("model")
 model.compile(optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'], loss=tf.keras.losses.CategoricalCrossentropy())
 model.fit(x=batch("train"), epochs=20, steps_per_epoch=104, validation_data=batch("val"), validation_steps=26, callbacks=[tensorboard_callback])
-model.save("model")
+model.save("model")'''
+
+model = tf.keras.models.load_model("model")
+for x, y in batch("val"):    
+    predicted = model.predict(x)
+    result = np.absolute(y-predicted)
+    for i in range(len(x)):
+        if np.argmax(predicted[i]) != np.argmax(y[i]):
+            mag, ang = x[i, :, :, 0], x[i, :, :, 1]
+            print("displaying image")
+            print("prediction:", encoding[np.argmax(predicted[i])], "expected:", encoding[np.argmax(y[i])])
+            display_flow(mag, ang)
+        else:
+            print("got it right!")
